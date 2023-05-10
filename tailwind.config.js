cc857cd172314784172e986ba32842a37c2692c7
SonMooSans <god63820869@gmail.com>
2023-05-10 22:41:54 +0800

    Add mdx files to Tailwind css content

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
         "./pages/**/*.{ts,tsx}",
         "./components/**/*.{ts,tsx}",
         "./app/**/*.{ts,tsx}",
+        "./docs/**/*.{mdx}",
     ],
     theme: {
         container: {
