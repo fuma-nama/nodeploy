c62bef6407ebc386e43020c487936a5381032a07
SonMooSans <god63820869@gmail.com>
2023-05-11 21:15:39 +0800

    Create Card component for mdx documenation

---
+++ components/mdx/card.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export function Cards({ children }: { children: ReactNode }) {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 not-prose">
+            {children}
+        </div>
+    );
+}
+
+export function Card({
+    href,
+    title,
+    description,
+}: {
+    href: string;
+    title: string;
+    description: string;
+}) {
+    return (
+        <Link
+            href={href}
+            target="_blank"
+            rel="noreferrer"
+            className="rounded-xl p-4 border-[1px] flex flex-col gap-2 hover:border-purple-400"
+        >
+            <h3 className="text-foreground font-semibold">{title}</h3>
+            <p className="text-muted-foreground text-sm">{description}</p>
+        </Link>
+    );
+}
