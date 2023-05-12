5cc0fd4f7b7bf644fa12dc949025908c43fa76b3
SonMooSans <god63820869@gmail.com>
2023-05-12 18:07:52 +0800

    Light theme for globe

--- app/(website)/earth.tsx
+++ app/(website)/earth.tsx
@@ -2,8 +2,10 @@
 
 import { ComponentPropsWithoutRef, useEffect, useRef } from "react";
 import createGlobe from "cobe";
+import { useTheme } from "next-themes";
 
 export function Earth(props: ComponentPropsWithoutRef<"canvas">) {
+    const { resolvedTheme } = useTheme();
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
@@ -16,11 +18,11 @@ export function Earth(props: ComponentPropsWithoutRef<"canvas">) {
             height: 900 * 2,
             phi: 0,
             theta: 0,
-            dark: 1,
+            dark: resolvedTheme === "dark" ? 1 : 0,
             diffuse: 1.2,
             mapSamples: 16000,
             mapBrightness: 6,
-            baseColor: [0.7, 0.3, 0.2],
+            baseColor: resolvedTheme === "dark" ? [0.7, 0.3, 0.2] : [1, 1, 1],
             markerColor: [0.98, 0.57, 0.24],
             glowColor: [0.98, 0.57, 0.24],
             markers: [],
@@ -33,7 +35,7 @@ export function Earth(props: ComponentPropsWithoutRef<"canvas">) {
         return () => {
             globe.destroy();
         };
-    }, []);
+    }, [resolvedTheme]);
 
     return <canvas ref={canvasRef} {...props} />;
 }
