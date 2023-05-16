03db9a94bc2ad964a0c8e2ed05d1dc129f734ee6
SonMooSans <god63820869@gmail.com>
2023-05-16 22:26:22 +0800

    Fix external links detecting on card component

--- components/mdx/card.tsx
+++ components/mdx/card.tsx
@@ -18,11 +18,13 @@ export function Card({
     title: string;
     description: string;
 }) {
+    const external = !href.startsWith("/");
+
     return (
         <Link
             href={href}
-            target="_blank"
-            rel="noreferrer"
+            target={external ? "_blank" : "_self"}
+            rel={external ? "noreferrer" : ""}
             className="rounded-xl p-4 border-[1px] flex flex-col gap-2 hover:border-purple-400"
         >
             <h3 className="text-foreground font-semibold">{title}</h3>
